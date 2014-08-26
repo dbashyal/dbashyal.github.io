b9a797e7406e94971b0259a09894f7dcfdde40ee
damu <damodar@acidgreen.com.au>
2014-08-26 17:40:44 +1000

    updated for magento.stackcircle.com

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,22 @@
-console.log('This would be the main JS file.');
+// Choose branch based on query string
+var query = window.location.search;
+if (query) {
+	var branch = query.replace("?", "");
+} else {
+	var branch = "master";
+}
+
+// Load readme content
+$.ajax({
+	url: "https://rawgit.com/dbashyal/Magento_PHP_Web_Developer/"+branch+"/readme.md",
+	dataType: 'text',
+	success: function(data) {
+
+		// Convert readme from markdown to html
+		var converter = new Markdown.Converter();
+
+		// Show html
+		$(".readme").html(converter.makeHtml(data));
+
+	}
+});
